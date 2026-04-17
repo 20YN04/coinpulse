2e0331d78f2be1a26238c2c03e77b27f17b7f9f3
20YN04 <Yentl.nerinckx@icloud.com>
2026-04-17 16:23:11 +0200

    feat: implement homepage layout with coin overview and categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
 
 const page = () => {
-  return <div></div>
+  return <main className='main-container'>
+    <section className='home-grid'>
+      <div id='coin-overview'>
+        <div className='header'>
+          <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" alt="Bitcoin" width={56} height={56} />
+          <div className='info'>
+            <p>Bitcoin / BTC</p>
+            <h1>$70,000.00</h1>
+          </div>
+        </div>
+
+      </div>
+
+      <p>Trending Coins</p>
+    </section>
+    <section className='w-full mt-7 space-y-4'>
+      <p>Categories</p>
+    </section>
+  </main>
 
 }
 
